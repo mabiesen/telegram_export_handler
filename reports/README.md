75702592dfae386dc2351878750d5d8e0777db8e
Matthew Biesen <mabiesen@outlook.com>
2022-01-04 22:46:21 -0600

    brainstorm

---
+++ reports/README.md
@@ -0,0 +1,9 @@
+# PURPOSE
+
+The reports directory houses python-scripted reports
+
+The report manager ingests a ChatMessageManager
+
+It runs the desired report - these will likely be functions
+
+It writes out data
--- script.py
+++ script.py
@@ -11,7 +11,7 @@ CMM = ChatMessageManager()
 CMM.load_messages_from_base_and_designated()
 
 print("Chat Message Manager Loaded To Global Variable 'CMM'\n\n")
-print("You will primarily be interacting with the ChatMessageManager's obtained messages - this is a ChatMessageCollection object.  This class is 'list-y', allowing one to access contained messages as-if it were a list.  See chat_message_collection interactor for further details")
+print("You will primarily be interacting with the ChatMessageManager's obtained messages - this is a ChatMessageCollection object.  This class is 'list-y', allowing one to access contained messages as-if it were a list.  See chat_message_collection interactor for further details\n\n")
 
 if __name__ == "__main__":
     args = sys.argv
