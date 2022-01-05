26822c906a8bcb05d913934bfa09815fc9afdc64
Matthew Biesen <mabiesen@outlook.com>
2022-01-04 22:10:15 -0600

    change

--- script.py
+++ script.py
@@ -1,6 +1,10 @@
 from chat_message_manager import ChatMessageManager
 
+import code
+import sys
+
 # Purpose of this script is to easily invoke ChatMessageManager on import
+# And eventually, easily run reports/queries
 # Speeds up python console testing; additionally, we almost always will use default file/directory values
 
 CMM = ChatMessageManager()
@@ -8,3 +12,10 @@ CMM.load_messages_from_base_and_designated()
 
 print("Chat Message Manager Loaded To Global Variable 'CMM'\n\n")
 print("You will primarily be interacting with the ChatMessageManager's obtained messages - this is a ChatMessageCollection object.  This class is 'list-y', allowing one to access contained messages as-if it were a list.  See chat_message_collection interactor for further details")
+
+if __name__ == "__main__":
+    args = sys.argv
+    if args[1] == 'report':
+        CMM.run_report(args[2])
+    elif args[1] == 'console':
+        code.interact(local=dict(globals(), **locals()))
